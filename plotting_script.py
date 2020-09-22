e8d1f99a3915ab2f98306530ed75c5770318b331
amontalb <alyssa.montalbano@cern.ch>
2020-09-22 13:53:25 -0400

    update address formatting

--- plotting_script.py
+++ plotting_script.py
@@ -3,6 +3,10 @@ import simplejson as json
 import pandas as pd
 from bokeh.io import curdoc
 from bokeh.plotting import figure, output_file, show
+from bokeh.resources import CDN
+from bokeh.embed import file_html
+
+#heroku app named: young-coast-81847
 
 def getData():
 
@@ -11,8 +15,7 @@ def getData():
     symbol = 'QCOM'
     outputsize = 'compact'
     apikey = 'Q4Q8JUAPXYPNCYOF'
-    address = 'https://www.alphavantage.co/query?function=' + function + \
-        '&symbol=' + symbol + '&outputsize=' + outputsize + '&apikey=' + apikey
+    address = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&outputsize={outputsize}&apikey={apikey}'
 
     r = requests.get(address).json()
     dates =(r['Time Series (Daily)'])
@@ -23,7 +26,9 @@ def getData():
     result = result.reset_index()
     result.columns = ['Date','open','high','low','close','adjusted close',\
         'volume','divident amount','split coefficient']
+    result['Date'] = pd.to_datetime(result['Date'],format = '%Y-%m-%d')
     print(result)
+    #possibly adjust Date to datetime
     return result
 
     #result = json.loads(r.content.decode('utf-8'))
@@ -32,12 +37,35 @@ def getData():
 #plot data with bokeh
 
 def plotData(data):
-    output_file('test_graph.html')
+    output_file('test_graph.html',title='test_graph',mode='cdn')
+
+    x = data['Date']
+    y0 = data['open']
+    y1 = data['close']
+    y2 = data['adjusted close']
+
+    #figure out y-range
+    data['high'] = pd.to_numeric(data.high)
+    maxY = data['high'].max()
+    data['low'] = pd.to_numeric(data.low)
+    minY = data['low'].min()
+
+    p = figure(tools = 'pan,box_zoom,reset,save', \
+        y_axis_label = 'price (USD)', title = 'Stocks', \
+        x_axis_label = 'Date', x_axis_type='datetime', \
+        y_range = [minY, maxY])
+    p.line(x, y0, legend_label = 'open')
+    p.line(x, y1, legend_label = 'close', color = 'firebrick')
+    p.line(x, y2, legend_label = 'adjusted close', color = 'red')
+
+    #show(p)
+    html = file_html(p, CDN, "stock ticker")
+    show(p)
 
 
 def main():
     data = getData()
-    plotData(data)
+#    plotData(data)
     #pushToApp()
 
 if __name__=="__main__":
