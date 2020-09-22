e2eb7268096769dc7155bc35c95fdefbf2d427d5
amontalb <alyssa.montalbano@cern.ch>
2020-09-22 14:57:21 -0400

    slight modifications

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import bokeh
 
 app = Flask(__name__)
 
@@ -9,6 +10,21 @@ def index():
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+#@app.route('/bokeh')
+#def bokeh():
+#    fig = figure(plot_width=600, plot_height=600)
+#    fig.vbar(x=[1, 2, 3, 4],width=0.5,bottom=0,top=[1.7, 2.2, 4.6, 3.9])
+#    js_resources = INLINE.render_js()
+#    css_resources = INLINE.render_css()
+#    script, div = components(fig)
+#    html = render_template(
+#        'index.html',
+#        plot_script=script,
+#        plot_div=div,
+#        js_resources=js_resources,
+#        css_resources=css_resources,)
+#    return encode_utf8(html)
 
 if __name__ == '__main__':
   app.run(port=33507)
