6cb4235fa44c4802d77c2ce3e6b16799000ab18c
amontalb <alyssa.montalbano@cern.ch>
2020-09-22 23:10:04 -0400

    add some plots, hopefully

--- app.py
+++ app.py
@@ -1,15 +1,67 @@
 from flask import Flask, render_template, request, redirect
-import bokeh
+import requests
+import simplejson as json
+import pandas as pd
+from bokeh.io import curdoc
+from bokeh.plotting import figure, output_file, show
+from bokeh.resources import CDN
+from bokeh.embed import file_html
 
 app = Flask(__name__)
 
 @app.route('/')
+def main():
+    return redirect('/index')
+
+@app.route('/index')
 def index():
   return render_template('index.html')
 
 @app.route('/about')
 def about():
   return render_template('about.html')
+
+ @app.route('/bokehPlot', methods=['POST'])
+ def bokehPlot():
+     # get data
+     function = 'TIME_SERIES_DAILY_ADJUSTED'
+     symbol = 'QCOM'
+     outputsize = 'compact'
+     apikey = 'Q4Q8JUAPXYPNCYOF'
+     address = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&outputsize={outputsize}&apikey={apikey}'
+
+     r = requests.get(address).json()
+     dates =(r['Time Series (Daily)'])
+
+     data = pd.DataFrame.from_dict(dates)
+     result = data.T
+     #result['Date'] = result.index.values.tolist()
+     result = result.reset_index()
+     result.columns = ['Date','open','high','low','close','adjusted close',\
+         'volume','divident amount','split coefficient']
+     result['Date'] = pd.to_datetime(result['Date'],format = '%Y-%m-%d')
+     #print(result)
+     x = data['Date']
+     y0 = data['open']
+     y1 = data['close']
+     y2 = data['adjusted close']
+
+     data['high'] = pd.to_numeric(data.high)
+     maxY = data['high'].max()
+     data['low'] = pd.to_numeric(data.low)
+     minY = data['low'].min()
+
+     p = figure(tools = 'pan,box_zoom,reset,save', \
+         y_axis_label = 'price (USD)', title = 'Stocks', \
+         x_axis_label = 'Date', x_axis_type='datetime', \
+         y_range = [minY, maxY])
+     if request.form.get('open'):
+         p.line(x, y0, legend_label = 'open')
+     if request.form.get('close'):
+         p.line(x, y1, legend_label = 'close', color = 'firebrick')
+     if request.form.get('adjusted close'):
+         p.line(x, y2, legend_label = 'adjusted close', color = 'red')
+     html = render_template('bokehPlot.html',)
 
 #open, close, adjusted close (high, low, volume)
 
